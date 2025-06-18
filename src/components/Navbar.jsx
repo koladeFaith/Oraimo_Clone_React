bd0aa25559642f70f8041860f0f33d598e6091ce
Kolade Faith <koladefaith1000@gmail.com>
2025-06-18 11:22:44 +0100

    Work done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,18 @@ import logoImg from '../images/logo_2_1200x1200 (1).png'
 
 const Navbar = () => {
   return (
+    <div>
+      <img src={logoImg} alt="" />
+      <i className="bi bi-search"></i>
+      <i className="bi bi-list"></i>
       <div>
-          <img src={logoImg} alt="" />
-          <div>
-              <li>Home</li>
-              <li>Products</li>
-              <li>Community</li>
-              <li>Support</li>
-          </div>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Community</li>
+        <li>Support</li>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
