710251a01a2dc12dc7b36a4e7d73f59e66bdafbb
Kolade Faith <koladefaith1000@gmail.com>
2025-06-20 13:43:12 +0100

    Work done

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -11,24 +11,43 @@ const Collection = () => {
       <h4 className="text-center py-4 fs-2">Collection list</h4>
       <div className={style.imgFlex}>
         <div>
-          <img src={img1} alt="" className={style.imgs}/>
+          <img src={img1} alt="" className={style.imgs} />
           <h5 className="fs-5 pt-1">Stick Vacuum Cleaner</h5>
           <p>1 products</p>
         </div>
         <div>
-          <img src={img2} alt="" className={style.imgs}/>
+          <img src={img2} alt="" className={style.imgs} />
           <h5 className="fs-5 pt-1">Spin Scrubber</h5>
           <p>0 products</p>
         </div>
         <div>
-          <img src={img3} alt="" className={style.imgs}/>
+          <img src={img3} alt="" className={style.imgs} />
           <h5 className="fs-5 pt-1">Humidifiers</h5>
           <p>0 products</p>
         </div>
         <div>
-          <img src={img4} alt="" className={style.imgs}/>
+          <img src={img4} alt="" className={style.imgs} />
           <h5 className="fs-5 pt-1">Accessories</h5>
           <p>6 products</p>
+        </div>
+      </div>
+      <div>
+        <div className={style.oriamo}>
+          <div>
+            {" "}
+            <h5 className="fw-300 fs-1 pb-3">
+              Oraimo Cordless 24KPA Vacuum Cleaner
+            </h5>
+            <li>Up to 24kpa Suction</li>
+            <li>Larger LED Display</li>
+            <li>Longer Runtime& Faster Charging</li>
+            <li>Tangle Defender Brush</li>
+            <li>Thoughtful Design</li>
+            <button>Learn more</button>
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
         </div>
       </div>
     </div>
