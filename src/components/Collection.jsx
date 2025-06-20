40e55e416ced904fc46d470bb5a59f9ab8d85732
Kolade Faith <koladefaith1000@gmail.com>
2025-06-20 13:04:58 +0100

    Work done

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -8,27 +8,27 @@ import img4 from "../images/OHV-102_180x.jpg";
 const Collection = () => {
   return (
     <div className={style.container}>
-        <h4 className="text-center py-4 fs-2">Collection list</h4>
+      <h4 className="text-center py-4 fs-2">Collection list</h4>
       <div className={style.imgFlex}>
         <div>
-          <img src={img1} alt="" />
+          <img src={img1} alt="" className={style.imgs}/>
           <h5 className="fs-5 pt-1">Stick Vacuum Cleaner</h5>
           <p>1 products</p>
         </div>
         <div>
-          <img src={img2} alt="" />
-          <h5 className="fs-5 pt-1">Stick Vacuum Cleaner</h5>
-          <p>1 products</p>
+          <img src={img2} alt="" className={style.imgs}/>
+          <h5 className="fs-5 pt-1">Spin Scrubber</h5>
+          <p>0 products</p>
         </div>
         <div>
-          <img src={img3} alt="" />
-          <h5 className="fs-5 pt-1">Stick Vacuum Cleaner</h5>
-          <p>1 products</p>
+          <img src={img3} alt="" className={style.imgs}/>
+          <h5 className="fs-5 pt-1">Humidifiers</h5>
+          <p>0 products</p>
         </div>
         <div>
-          <img src={img4} alt="" />
-          <h5 className="fs-5 pt-1">Stick Vacuum Cleaner</h5>
-          <p>1 products</p>
+          <img src={img4} alt="" className={style.imgs}/>
+          <h5 className="fs-5 pt-1">Accessories</h5>
+          <p>6 products</p>
         </div>
       </div>
     </div>
